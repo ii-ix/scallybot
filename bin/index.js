1b1f58b68cf4dc2df77a28930af6f88665e06bf0
II IX <ii-ix-bot@protonmail.com>
2023-04-12 15:29:02 -0400

    Add commonJS `index.js`

---
+++ bin/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const {readdir} = require("fs");
+const {PREFIX} = require("../meta/config.json");
+const {Collection, Client} = require("discord.js");
+const client = new Client();
+client.commands = new Collection();
+client.queue = new Map()
+
+client.config = {
+    prefix: PREFIX || process.env.PREFIX
+}
+
+async function loader(feature, client) {
+    const actions = ['commands', 'events'];
+    const rootDir = `./src/${feature}`
+    for await (const action of actions) {
+        const actionDir = `${rootDir}/${action}`;
+        await readdir(actionDir, (err, files) => {
+            if (err) console.error(err.message)
+            switch (action) {
+                case 'commands':
+                    files.forEach(file => {
+                        if (!file.endsWith(".js")) return;
+                        let props = require(`../${rootDir}/${action}/${file}`);
+                        let commandName = file.split(".")[0];
+                        client.commands.set(commandName, props);
+                        console.log(`Loading Command: ${commandName}`)
+                    })
+                    break;
+                case 'events':
+                    files.forEach(file => {
+                        const event = require(`../${actionDir}/${file}`)
+                        let eventName = file.split(".")[0];
+                        client.on(eventName, event.bind(this, client));
+                        console.log(`Loading Event: ${eventName}`)
+                    });
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
+}
+
+(async () => {
+    await loader('music', client)
+    await client.login(process.env.TOKEN)
+})()
