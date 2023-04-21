4700ab8df41bcfff7c64479014e3151c5c53d4d3
II IX (Two Nine) <ii-ix-bot@protonmail.com>
2023-04-20 20:21:39 -0400

    Adds more intents

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,12 @@ const {readdir} = require("fs");
 const {PREFIX} = require("../meta/config.json");
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent
+    ],
   });
 client.commands = new Collection();
 client.queue = new Map()
