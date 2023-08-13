363974c5b023f0b9b7bfc030b8f79e9c0a7442b6
II IX <ii-ix-bot@protonmail.com>
2023-08-12 22:19:35 -0400

    Minor formatting and syntax changes

--- bin/index.mjs
+++ bin/index.mjs
@@ -6,11 +6,18 @@ import config from "../meta/config.json" assert { type: "json" };
 import { getDirectoryName } from "../src/utils/getDirectoryName.mjs";
 import validateFunctionModule from "../src/utils/validateFunctionModule.mjs";
 
+dotenv.config()
+const __dirname = getDirectoryName(import.meta.url);
+const commands = [];
+
 (async () => {
-    const __dirname = getDirectoryName(import.meta.url);
-    const commands = [];
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+    await client.login(process.env.DISCORD_BOT_TOKEN)
+    client.commands = new Collection();
+
     const foldersPath = join(__dirname, "../src/commands");
     const commandFolders = readdirSync(foldersPath);
+
     for (const folder of commandFolders) {
         const commandsPath = join(foldersPath, folder);
         const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith(".mjs"));
@@ -19,10 +26,25 @@ import validateFunctionModule from "../src/utils/validateFunctionModule.mjs";
                 const filePath = join(commandsPath, file);
                 const { default: commandModule } = await import(filePath);
                 await validateFunctionModule(commandModule, file);
-                console.log(`The module '${file}' passed.`);
+                client.commands.set(commandModule.data.name, commandModule);
             } catch (error) {
                 console.error(error.message);
             }
         }
     }
+    // const eventsPath = path.join(__dirname, "../src/events");
+    // const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".mjs"));
+
+    // for (const file of eventFiles) {
+    //     const filePath = path.join(eventsPath, file);
+    //     const event = require(filePath);
+    //     if (event.once) {
+    //         client.once(event.name, (...args) => event.execute(...args));
+    //     } else {
+    //         client.on(event.name, (...args) => event.execute(...args));
+    //     }
+    // }
+
+    client.login(process.env.DISCORD_BOT_TOKEN);
+
 })()
