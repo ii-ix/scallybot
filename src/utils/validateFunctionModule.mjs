54d440fe84ee1bf65adbae9e4c08fd183893afea
II IX <ii-ix-bot@protonmail.com>
2023-08-12 21:36:44 -0400

    Adds module validation for objects with properties `data` & `execute`
    Adds module validation for objects with properties `data` & `execute`

---
+++ src/utils/validateFunctionModule.mjs
@@ -0,0 +1,13 @@
+export default async function validateFunctionModule(moduleToValidate) {
+    if (typeof moduleToValidate !== 'object') {
+        throw new Error('The provided module is not an object.');
+    }
+    const requiredProperties = ['data', 'execute'];
+    const missingProperties = requiredProperties.filter(prop => !moduleToValidate[prop]);
+    if (missingProperties.length > 0) {
+        const missingPropsString = missingProperties.join(', ');
+        const errorText = missingProperties.length === 1 ? 'property is missing' : 'properties are missing';
+        throw new Error(`The provided module is missing the required ${errorText}: ${missingPropsString}`);
+    }
+    return true; // Validation passed
+}
