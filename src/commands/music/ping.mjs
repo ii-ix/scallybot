61a4f53c5ef8277a625b43ea751784cb3877330a
II IX <ii-ix-bot@protonmail.com>
2023-08-12 22:19:48 -0400

    Fix Demo Slash Command

--- src/commands/music/ping.mjs
+++ src/commands/music/ping.mjs
@@ -1,13 +1,9 @@
 import { SlashCommandBuilder } from "discord.js"
 
 export default {
-    data: new SlashCommandBuilder({
-        options: {
-            name: "Ping",
-            description: "Replies with 'Pong'",
-            type: String
-        }
-    }),
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
     async execute(interaction) {
         await interaction.reply('Pong')
     }
