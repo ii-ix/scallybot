8d16ec9eeb0381e62439ff136979b6bfab16af5a
II IX <ii-ix-bot@protonmail.com>
2023-08-12 21:41:08 -0400

    Syntax updates

--- src/music/events/message.mjs
+++ src/music/events/message.mjs
@@ -38,37 +38,34 @@
 // };
 import { EventEmitter } from 'events';
 
-const eventHandler = (client, message) => {
+export function eventHandler(client, message) {
     const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-    const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
-    if (message.content.indexOf(prefix) === 0) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        //Making the command lowerCase because our file name will be in lowerCase
-        const command = args.shift().toLowerCase();
+    // const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
+    const prefix = client.config.prefix;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    //Making the command lowerCase because our file name will be in lowerCase
+    const command = args.shift().toLowerCase();
 
-        //Searching a command
-        const cmd = client.commands.get(command);   
-        //Searching a command aliases
-        const aliases = client.commands.find(x => x.info.aliases.includes(command))
+    //Searching a command
+    const cmd = client.commands.get(command);
+    //Searching a command aliases
+    const aliases = client.commands.find(x => x.info.aliases.includes(command))
 
-        if (message.channel.type === "dm") return message.channel.send("None of the commands work in DMs. So please use commands in server!")
-        process.on("unhandledRejection", (reason, promise) => {
-            try {
-                console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);
-            } catch {
-                console.error(reason);
-            }
-        });
-        
-        EventEmitter.defaultMaxListeners = 25
-
-        //Executing the codes when we get the command or aliases
-        if (cmd) {
-            cmd.run(client, message, args);
-        } else if (aliases) {
-            aliases.run(client, message, args);
+    if (message.channel.type === "dm") return message.channel.send("None of the commands work in DMs. So please use commands in server!")
+    process.on("unhandledRejection", (reason, promise) => {
+        try {
+            console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);
+        } catch {
+            console.error(reason);
         }
+    });
+
+    EventEmitter.defaultMaxListeners = 25
+
+    //Executing the codes when we get the command or aliases
+    if (cmd) {
+        cmd.run(client, message, args);
+    } else if (aliases) {
+        aliases.run(client, message, args);
     }
 }
-
-export default eventHandler;
