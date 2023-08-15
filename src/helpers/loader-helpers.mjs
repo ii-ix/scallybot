6b9b6c15a255b9e03084618e3b5f972544e7bf24
II IX <ii-ix-bot@protonmail.com>
2023-08-14 23:53:56 -0400

    Simplifies `loadCommandOrEvent` to handle single file and type

--- src/helpers/loader-helpers.mjs
+++ src/helpers/loader-helpers.mjs
@@ -1,30 +1,16 @@
-import { readdirSync } from "fs";
-import { join } from "path";
-import { getDirectoryName } from "./directory-helpers.mjs";
 import { validateFunctionModule } from "./validation-helpers.mjs";
-import { deployCommands } from "./command-helpers.mjs"
-import config from "../../meta/config.json" assert { type: "json" };
 
-export async function loadCommandOrEvent(client, folderName) {
+export async function loadCommandOrEvent(client, file, type) {
   const commands = []
-  const __dirname = getDirectoryName(import.meta.url);
-  const folderPath = join(__dirname, `../../src/${folderName}`);
-  const files = readdirSync(folderPath, { recursive: true }).filter(file => file.endsWith(".mjs"));
-
-  for (const file of files) {
-    try {
-      const filePath = join(folderPath, file);
-      const { default: module } = await import(filePath);
-      await validateFunctionModule(module, file);
-      client[folderName].set(module.data.name, module);
-      if (updateCommands) commands.push(module.data.toJSON())
-    } catch (error) {
-      console.error(error.message);
+  try {
+    const { default: module } = await import(file);
+    const isValidModule = await validateFunctionModule(module, file);
+    if (isValidModule) {
+      client[type].set(module.data.name, module);
+      return module.data
     }
-  }
-  if (config.updateCommand) {
-    const { clientId, guildId } = config
-    const token = process.env.DISCORD_BOT_TOKEN
-    await deployCommands(clientId, guildId, token, commands)
+    return {}
+  } catch (error) {
+    console.error(error.message);
   }
 }
