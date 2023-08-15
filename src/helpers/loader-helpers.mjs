b3376b8ed7dc72e51f8dc8d7a3172bf4231c08c4
II IX <ii-ix-bot@protonmail.com>
2023-08-14 20:38:34 -0400

    Adds updateCommand conditional to `loader-helpers.mjs`

--- src/helpers/loader-helpers.mjs
+++ src/helpers/loader-helpers.mjs
@@ -1,12 +1,16 @@
 import { readdirSync } from "fs";
 import { join } from "path";
-import { getDirectoryName } from "./getDirectoryName.mjs";
-import { validateFunctionModule } from "./validateFunctionModule.mjs";
+import { REST, Routes } from "discord.js";
+import { getDirectoryName } from "./directory-helpers.mjs";
+import { validateFunctionModule } from "./validation-helpers.mjs";
+import { deployCommands } from "./command-helpers.mjs"
+import config from "../../meta/config.json" assert { type: "json" };
 
 export async function loadCommandOrEvent(client, folderName) {
+  const commands = []
   const __dirname = getDirectoryName(import.meta.url);
   const folderPath = join(__dirname, `../../src/${folderName}`);
-  const files = readdirSync(folderPath).filter(file => file.endsWith(".mjs"));
+  const files = readdirSync(folderPath, { recursive: true }).filter(file => file.endsWith(".mjs"));
 
   for (const file of files) {
     try {
@@ -14,8 +18,10 @@ export async function loadCommandOrEvent(client, folderName) {
       const { default: module } = await import(filePath);
       await validateFunctionModule(module, file);
       client[folderName].set(module.data.name, module);
+      if (updateCommands) commands.push(module.data.toJSON())
     } catch (error) {
       console.error(error.message);
     }
   }
+  if (config.updateCommand) await deployCommands(config.clientId, guildId, commands)
 }
--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
-import { Client, Collection, GatewayIntentBits } from "discord.js";
-import { loadCommandOrEvent } from "./helpers/commandLoader.mjs";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { loadCommandOrEvent } from "./helpers/loader-helpers.mjs";
 
 (async () => {
     dotenv.config()
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-    await client.login(process.env.DISCORD_BOT_TOKEN)
     client.commands = new Collection();
     const folders = ["commands", "events"]
-    for (const folder of folders) {
-        await loadCommandOrEvent(client, folder)
-    }
+    for (const folder of folders) await loadCommandOrEvent(client, folder)
+    client.once(Events.ClientReady, () => {
+        console.log('Ready!');
+    });
     client.login(process.env.DISCORD_BOT_TOKEN);
 })()
