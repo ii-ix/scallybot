a4b8291cb914c666f4137c76824f7195905e6c2b
II IX <ii-ix-bot@protonmail.com>
2023-08-14 23:52:02 -0400

    Adds support for recursively reading modules

--- src/helpers/directory-helpers.mjs
+++ src/helpers/directory-helpers.mjs
@@ -1,7 +1,21 @@
 import { fileURLToPath } from 'url';
-import path from 'path';
+import { readdirSync } from "fs";
+import { dirname, join } from 'path';
 
 export function getDirectoryName(directory) {
   const currentModulePath = fileURLToPath(directory);
-  return path.dirname(currentModulePath);
+  return dirname(currentModulePath);
+}
+
+export function readFilesRecursively(directory) {
+  let files = [];
+  const items = readdirSync(directory, { withFileTypes: true });
+  items.forEach(item => {
+    const fullPath = join(directory, item.name);
+    if (item.isDirectory()) {
+      const nestedFiles = readFilesRecursively(fullPath);
+      files = files.concat(nestedFiles);
+    } else if (item.isFile() && item.name.endsWith(".mjs")) files.push(fullPath)
+  });
+  return files;
 }
