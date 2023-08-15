18ddcf2946cae1e61cb47981d2731ca4e4e50ca2
II IX <ii-ix-bot@protonmail.com>
2023-08-14 23:55:28 -0400

    Updates validateFunctionModule to return a boolean value

--- src/helpers/validation-helpers.mjs
+++ src/helpers/validation-helpers.mjs
@@ -1,13 +1,21 @@
 export async function validateFunctionModule(moduleToValidate) {
-    if (typeof moduleToValidate !== 'object') {
-        throw new Error('The provided module is not an object.');
+    try {
+        if (typeof moduleToValidate !== 'object') {
+            throw new Error('The provided module is not an object.');
+        }
+
+        const requiredProperties = ['data', 'execute'];
+        const missingProperties = requiredProperties.filter(prop => !moduleToValidate[prop]);
+
+        if (missingProperties.length > 0) {
+            const missingPropsString = missingProperties.join(', ');
+            const errorText = missingProperties.length === 1 ? 'property is missing' : 'properties are missing';
+            throw new Error(`The provided module is missing the required ${errorText}: ${missingPropsString}`);
+        }
+
+        return true;
+    } catch (error) {
+        console.error(error.message);
+        return false;
     }
-    const requiredProperties = ['data', 'execute'];
-    const missingProperties = requiredProperties.filter(prop => !moduleToValidate[prop]);
-    if (missingProperties.length > 0) {
-        const missingPropsString = missingProperties.join(', ');
-        const errorText = missingProperties.length === 1 ? 'property is missing' : 'properties are missing';
-        throw new Error(`The provided module is missing the required ${errorText}: ${missingPropsString}`);
-    }
-    return true;
 }
