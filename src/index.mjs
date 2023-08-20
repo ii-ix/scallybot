312e53d206e8e7b759947895b76d6b5b0bab174b
II IX <ii-ix-bot@protonmail.com>
2023-08-20 14:15:07 -0400

    Updates `meta/` to `config/` & comments out `index.mjs`

--- src/index.mjs
+++ src/index.mjs
@@ -3,48 +3,49 @@ import { join } from "path";
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import { loadCommandOrEvent } from "./helpers/loader-helpers.mjs";
 import { deployCommands } from "./helpers/command-helpers.mjs"
-import config from "../meta/config.json" assert { type: "json" };
+import config from "../config/config.json" assert { type: "json" };
 import { getDirectoryName, readFilesRecursively } from "./helpers/directory-helpers.mjs";
 
-(async () => {
-    dotenv.config()
-    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-    client.commands = new Collection();
-    const folders = ["commands", "events"]
-    const commandsOrEvents = []
-    for (const folder of folders) {
-        const __dirname = getDirectoryName(import.meta.url);
-        const folderPath = join(__dirname, `../src/${folder}`);
-        const files = readFilesRecursively(folderPath)
-        for (const file of files) {
-            const commandOrEvent = await loadCommandOrEvent(client, file, folder)
-            commandsOrEvents.push(commandOrEvent)
-        }
-    }
-    const { updateCommands } = config
-    const token = process.env.DISCORD_BOT_TOKEN
-    if (updateCommands) {
-        const { clientId, guildId } = config
-        await deployCommands(clientId, guildId, token, commandsOrEvents)
-    }
-    client.once(Events.ClientReady, () => {
-        console.log('Ready!');
-    });
 
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-        const command = client.commands.get(interaction.commandName);
-        if (!command) return;
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-    });
-    client.login(token);
-})()
+// (async () => {
+//     dotenv.config()
+//     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//     client.commands = new Collection();
+//     const types = ["commands", "events"]
+//     const commandsAndEvents = []
+//     for (const type of types) {
+//         const __dirname = getDirectoryName(import.meta.url);
+//         const folderPath = join(__dirname, `../src/${type}`);
+//         const files = readFilesRecursively(folderPath)
+//         for (const file of files) {
+//             const commandOrEvent = await loadCommandOrEvent(client, file, type)
+//             commandsAndEvents.push(commandOrEvent)
+//         }
+//     }
+//     const { updateCommands } = config
+//     const token = process.env.DISCORD_BOT_TOKEN
+//     if (updateCommands) {
+//         const { clientId, guildId } = config
+//         await deployCommands(clientId, guildId, token, commandsAndEvents)
+//     }
+//     client.once(Events.ClientReady, () => {
+//         console.log('Ready!');
+//     });
+
+//     client.on(Events.InteractionCreate, async interaction => {
+//         if (!interaction.isChatInputCommand()) return;
+//         const command = client.commands.get(interaction.commandName);
+//         if (!command) return;
+//         try {
+//             await command.execute(interaction);
+//         } catch (error) {
+//             console.error(error);
+//             if (interaction.replied || interaction.deferred) {
+//                 await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+//             } else {
+//                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+//             }
+//         }
+//     });
+//     client.login(token);
+// })()
