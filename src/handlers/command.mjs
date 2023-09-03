44244e0fac76127cb3923beebc5e7c8ca711c086
II IX <ii-ix-bot@protonmail.com>
2023-09-02 21:40:13 -0400

    Updates the module import files & replaces function named to default

--- src/handlers/command.mjs
+++ src/handlers/command.mjs
@@ -1,17 +1,17 @@
-import { log } from '../functions.js';
-import ExtendedClient from '../class/ExtendedClient.js';
+import { log } from '../functions/index.mjs';
+import ExtendedClient from '../class/ExtendedClient.mjs';
 import { getModuleFilesRecursively, loadCommandOrEvent } from '../functions/index.mjs';
 
 /**
  * 
  * @param {ExtendedClient} client 
  */
-export default async function loadCommands(client) {
+export default async (client) => {
     const types = ['prefix'];
     types.forEach(async type => {
-        const files = getModuleFilesRecursively(`../commands/${type}`);
+        const files = getModuleFilesRecursively(`./src/commands/${type}`);
         files.forEach(async file => {
-            await loadCommandOrEvent(file);
+            await loadCommandOrEvent(client, file, type);
             const logMessage = `Loaded new command: ${file}`;
             log(logMessage, 'info');
         });
