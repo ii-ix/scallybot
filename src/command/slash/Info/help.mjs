810afd5f0c78998e7111b7f6d2a9fa2d4ea5bf35
II IX (Two Nine) <ii-ix-bot@protonmail.com>
2023-09-26 16:20:51 -0400

    Adds a dummy `slash` command

---
+++ src/command/slash/Info/help.mjs
@@ -0,0 +1,29 @@
+import { Message, EmbedBuilder } from 'discord.js';
+import ExtendedClient from '../../../class/ExtendedClient.mjs';
+import config from '../../../../config/config.json' assert { type: 'json' };
+// const GuildSchema = require('../../../schemas/GuildSchema');
+
+export default {
+    data: {
+        name: 'help',
+        description: 'The help menu for all possible commands.',
+        aliases: ['h'],
+        cooldown: 15000
+    },
+    async execute(client, message, args) {
+        let prefix = config.handler.prefix;
+        // if (config.handler?.mongodb?.toggle)
+        const mapIntCmds = client.applicationcommandsArray.map((v) => `\`/${v.name}\`: ${v.description || '(No description)'}`);
+        const mapPreCmds = client.collection.prefixcommands.map((v) => `\`${prefix}${v.data.name}\` (${v.data.aliases.length > 0 ? v.data.aliases.map((a) => `**${a}**`).join(', ') : 'None'}): ${v.data.description || '(No description)'}`);
+        await message.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('Help command')
+                    .addFields(
+                        { name: 'Slash commands', value: `${mapIntCmds.join('\n')}` },
+                        { name: 'Prefix commands', value: `${mapPreCmds.join('\n')}` }
+                    )
+            ]
+        });
+    }
+}
