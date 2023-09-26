89afa306a4c4c0e1566b92e42fc387b452f25fe1
II IX (Two Nine) <ii-ix-bot@protonmail.com>
2023-09-26 15:19:53 -0400

    Fixes error handling in `handler/index.mjs`

--- src/handlers/index.mjs
+++ src/handlers/index.mjs
@@ -6,25 +6,24 @@ import { getModuleFilesRecursively, loadCommandOrEvent, log } from '../functions
  * @param {ExtendedClient} client 
  */
 export default async (client) => {
-    try {
-        const types = ['prefix', 'slash', 'events'];
+    const types = ['prefix', 'slash', 'events'];
 
-        // Define a mapping of type to directory paths
-        const typeToDir = {
-            prefix: './src/command/prefix',
-            slash: './src/command/slash',
-            events: './src/event',
-        };
+    // Define a mapping of type to directory paths
+    const typeToDir = {
+        prefix: './src/command/prefix',
+        slash: './src/command/slash',
+        events: './src/event',
+    };
 
-        // Iterate through each type
-        for (const type of types) {
-            const directory = typeToDir[type];
+    // Iterate through each type
+    for (const type of types) {
+        try {
             const files = getModuleFilesRecursively(directory);
             // Load commands or events based on the type
-            files.forEach((file) => loadCommandOrEvent(client, file, type))
+            files.forEach((file) => loadCommandOrEvent(client, file, type));
+        } catch (error) {
+            const errorMessage = `Error initializing of type '${type}': ${error.message}`;
+            log(errorMessage, 'err');
         }
-    } catch (error) {
-        const errorMessage = `Error initializing commands and events: ${error.message}`;
-        log(errorMessage, 'err')
     }
 }
